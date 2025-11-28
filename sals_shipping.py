7c4875fa0eaebc0096a8b930a39f0a2a4ead5ba1
Marx <byt3marx@gmail.com>
2025-11-28 10:14:55 +0100

    updated use of functions to avoid repeating similar logic

--- sals_shipping.py
+++ sals_shipping.py
@@ -1,31 +1,35 @@
 weight = 41.5
 
 #Ground shipping
-if weight <= 0:
-  print("Please enter a valid positive weight.")
-elif weight <= 2:
-  cost_ground = weight * 1.5 + 20.0
-elif weight <= 6:
-  cost_ground = weight * 3.0 + 20.0
-elif weight <= 10:
-  cost_ground = weight * 4.0 + 20.0
-else:
-  cost_ground = weight * 4.75 + 20.0
+def ground_shipping(weight):
+    if weight <= 0:
+      print("Please enter a valid positive weight.")
+    elif weight <= 2:
+      return weight * 1.5 + 20.0
+    elif weight <= 6:
+      return weight * 3.0 + 20.0
+    elif weight <= 10:
+      return weight * 4.0 + 20.0
+    else:
+      return weight * 4.75 + 20.0
+cost_ground = ground_shipping(weight)
 
 #Premium shipping
 cost_ground_premium = 125.00
 
 #Drone shipping
-if weight <= 0:
-  print("Please enter a valid positive weight.")
-elif weight <= 2:
-  cost_drone_shipping = weight * 4.5
-elif weight <= 6:
-  cost_drone_shipping = weight * 9.0
-elif weight <= 10:
-  cost_drone_shipping = weight * 12.0
-else:
-  cost_drone_shipping = weight * 14.25
+def drone_shipping(weight):
+  if weight <= 0:
+    print("Please enter a valid positive weight.")
+  elif weight <= 2:
+    return weight * 4.5
+  elif weight <= 6:
+    return weight * 9.0
+  elif weight <= 10:
+    return weight * 12.0
+  else:
+    return weight * 14.25
+cost_drone_shipping = drone_shipping(weight)
 
 print(f"Ground shipping cost of the package is: {cost_ground:.2f}€.")
 print(f"Ground premium shipping cost of the package is: {cost_ground_premium:.2f}€.")
